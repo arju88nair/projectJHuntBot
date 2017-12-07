69d5fe324ce30959c18dc77bd23af7e6ec405f29
arju88nair <arju88nair@gmail.com>
2017-12-07 20:58:34 +0530

    Database adding

--- PCscrapy/spiders/PCscrap.py
+++ PCscrapy/spiders/PCscrap.py
@@ -20,6 +20,10 @@ db = connection.Test
 
 
 class Spider(XMLFeedSpider):
+    """
+        Active main spider which crawls through the links provided
+
+    """
     name = "scrap"
     allowed_domains = ["feeds.feedburner.com"]
     itertag = 'item'
@@ -36,9 +40,16 @@ class Spider(XMLFeedSpider):
             logging.error('For ' + url[0] + ' in ' + url[2])
             yield request
 
+    """
+    
+    Parsing block for the default rss 
+    
+    """
+
     def parse_node(self, response, node):
         item = {}
         source = response.meta.get('source')
+        category = response.meta.get('category')
         title = node.xpath('title/text()').extract_first()
         item['title'] = title
         item['link'] = node.xpath('link/text()').extract_first()
@@ -47,19 +58,19 @@ class Spider(XMLFeedSpider):
         item['summary'] = cleanhtml(description)
         item['source'] = response.meta.get('source')
         if source == "The Guardian":
-            item['media'] = node.xpath("*[local-name()='content'][@width='460']/@url").extract_first()
+            item['image'] = node.xpath("*[local-name()='content'][@width='460']/@url").extract_first()
         else:
             media = node.xpath("*[local-name()='content']/@url").extract_first()
             thumb = node.xpath("*[local-name()='thumbnail']/@url").extract_first()
             full = node.xpath("fullimage/text()").extract_first()
             if media:
-                item['media'] = media
+                item['image'] = media
             elif thumb:
-                item['media'] = thumb
+                item['image'] = thumb
             elif full:
-                item['media'] = full
+                item['image'] = full
             else:
-                item['media'] = ''
+                item['image'] = ''
 
         item['category'] = response.meta.get('category')
         item['type'] = response.meta.get('type')
@@ -72,18 +83,48 @@ class Spider(XMLFeedSpider):
         for word in words:
             tagWordArray.append(word[0].title())
         item['tags'] = tagWordArray
-        db[response.meta.get('category')].insert_one(item)
+        insertingBlock(item, source, category)
 
     def handle_spider_closed(spider, reason):
-        logging.info('Work time:', time.time() - start)
+        logging.info('Work time:' + str(time.time() - start))
 
     dispatcher.connect(handle_spider_closed, signals.spider_closed)
 
 
 def cleanhtml(raw_html):
-    if (raw_html):
+    """
+    To remove html tags in the summary
+    """
+    if raw_html is not None:
         cleanr = re.compile('<.*?>')
         cleantext = re.sub(cleanr, ' ', raw_html)
         return cleantext
     else:
         return ""
+
+
+def insertingBlock(item, source, category):
+    """
+
+       Inserting  function with respect to the collection name parsed
+
+       """
+
+    if db[category].count() == 0:
+        db[category].insert_one(item)
+    else:
+        tags = str(item['uTag'])
+        if db.Main.find_one(
+                {'uTag': tags}, {'_id': 1}):
+            pass
+        else:
+            insertDoc = db.Main.insert_one(item)
+            db[category].insert_one(item)
+            if insertDoc:
+                logging.debug('Inserted new for ' + category + "   for  " + source
+                              )
+                logging.debug('\n')
+            else:
+                logging.debug('Error in insertion for ' +
+                              category + "   for  " + source)
+                logging.debug('\n')
