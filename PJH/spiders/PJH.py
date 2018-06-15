05a3cd7e5fff1d7cafc78fe4bbb768ef258cf918
arju88nair <arju88nair@gmail.com>
2018-06-15 21:22:59 +0530

    user agents for pc

--- PJH/spiders/PJH.py
+++ PJH/spiders/PJH.py
@@ -40,8 +40,8 @@ class Spider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+
+        filename = 'quotes-%s.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
